30cf8c37ef66681d61c90d6145a7a81ada320236
Balthiii <balthasalaise@gmail.com>
2024-03-25 10:24:23 +0100

    add ejs

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-import express from "express";
+import Express from "express";
 //Créer une application Express
-const app = express();
+const app = Express();
 
 //Configuration de mon application pour qu'elle puisse servir du contenu statique
-app.use(express.static("public"));
+app.use(Express.static('public'));
 
 // Configure mon application pour qu'elle utlise ejs comme moteur de templating
 // l'outil qui va generer de l'html
 app.set("view engine", "ejs");
 app.set("views", "views");
-
 
 
 app.get("/", (req, res) => {
