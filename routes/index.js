39e76de052d4cec6c3d3b020f02c7dfa1626828e
Balthiii <balthasalaise@gmail.com>
2024-05-18 20:45:31 +0200

    "feat : Added authentification system"

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const router = express.Router();
+
+import auth from '../config/checkAuth.js';
+
+//------------ Welcome Route ------------//
+router.get('/', (req, res) => {
+    res.render('welcome');
+});
+
+//------------ Dashboard Route ------------//
+router.get('/dashboard', auth.ensureAuthenticated, (req, res) => res.render('dash', {
+    name: req.user.name
+}));
+
+export default router;
