67a6b72b842ea92b42f7429f98e0224d69580f00
Balthiii <balthasalaise@gmail.com>
2024-06-07 09:52:46 +0200

    feature : connection to the TDMB api and storage of various films and series in our database

---
+++ models/Serie.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const SerieSchema = new mongoose.Schema({
+    title: String,
+    release_date: String,
+    vote_average: String,
+    vote_count: String,
+    overview: String,
+    runtime: String,
+    tagline: String,
+    backdrop_path: String,
+    poster_path: String,
+    genre: Array,
+    budget: String,
+    popularity: String,
+
+});
+
+const Serie = mongoose.model('Serie', SerieSchema);
+
+export default Serie;
