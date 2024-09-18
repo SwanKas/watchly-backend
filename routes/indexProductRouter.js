0740b62fcf34d42accf0e2a72c101ba83a7d7ade
Balthiii <balthasalaise@gmail.com>
2024-09-18 10:13:59 +0200

    nolog:updates files

--- routes/indexProductRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import indexAll from "../controllers/elasticSearchController.js";
-
-const router = express.Router();
-
-router.get("/index-product", indexAll);
-
-export default router;
